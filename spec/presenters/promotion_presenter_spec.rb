5758ddd83d3fbe85fae86444cc8991780bd75cbc
Campus Code 04 <carlosepalha@gmail.com>
2019-01-28 21:33:45 -0200

    implementa presenters

---
+++ spec/presenters/promotion_presenter_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe PromotionPresenter do
+  describe '#status'do
+    it 'it should render pending status' do
+      promotion = build(:promotion, status: :pending)
+      presenter = PromotionPresenter.new(promotion, nil)
+      expect(presenter.status).to include('<span class="ls-tag-warning">Pendente</span>')
+    end
+  end
+end
