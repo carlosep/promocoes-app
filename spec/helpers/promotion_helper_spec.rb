fe9c457818e4c409cfc79bd82e1f6b9d9b44a9cb
Campus Code 04 <campuscode04@Campuss-Mac-mini-6.local>
2019-01-28 20:24:56 -0200

    cria helper para status da promocao

---
+++ spec/helpers/promotion_helper_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe PromotionHelper do
+  describe 'promotion_status'do
+    it 'it should render pending status' do
+      promotion = build(:promotion, status: :pending)
+      status = promotion_status(promotion)
+      expect(status).to include('<span class="ls-tag-warning">Pendente</span>')
+    end
+    it 'it should render pending status' do
+      promotion = build(:promotion, status: :approved)
+      status = promotion_status(promotion)
+      expect(status).to eq('<span class="ls-tag-info">Aprovada</span>')
+    end
+    it 'it should render pending status' do
+      promotion = build(:promotion, status: :activated)
+      status = promotion_status(promotion)
+      expect(status).to eq('<span class="ls-tag-success">Ativado</span>')
+    end
+    it 'it should render pending status' do
+      promotion = build(:promotion, status: :expired)
+      status = promotion_status(promotion)
+      expect(status).to eq('<span class="ls-tag-alert">Expirada</span>')
+    end
+  end
+end
